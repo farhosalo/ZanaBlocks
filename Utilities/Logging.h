35af31e9228ed8eefbb4a08befb1ec18972d2069
Salo <info@salo.com.de>
2026-04-03 22:29:13 +0200

    Add logging macros

---
+++ Utilities/Logging.h
@@ -0,0 +1,41 @@
+#pragma once
+
+#include <ctime>
+#include <iostream>
+#include <string>
+
+auto constexpr RESET{"\033[0m"};
+auto constexpr WHITE{"\033[37m"};
+auto constexpr CYAN{"\033[36m"};
+auto constexpr YELLOW{"\033[33m"};
+auto constexpr RED{"\033[31m"};
+auto constexpr LIGHT_RED{"\033[91m"};
+auto constexpr PURPLE{"\033[35m"};
+
+inline std::string timestamp() {
+  const auto time = std::time(nullptr);
+  auto constexpr BUF_SIZE{20};
+  std::array<char, BUF_SIZE> buf{};
+  std::strftime(buf.data(), buf.size(), "%Y-%m-%d %H:%M:%S",
+                std::localtime(&time));
+
+  return std::string{buf.data()};
+}
+// NOLINTBEGIN (cppcoreguidelines-macro-usage)
+#define LOG(color, level, msg)                                            \
+  std::cout << color << "[" << timestamp() << "] "                        \
+            << "[" << level << "] "                                       \
+            << "[" << __FILE__ << ":" << __LINE__ << "] " << msg << RESET \
+            << "\n"
+
+#define INFO(msg) LOG(WHITE, "INFO ", msg)
+#define DEBUG(msg) LOG(CYAN, "DEBUG", msg)
+#define WARN(msg) LOG(YELLOW, "WARN ", msg)
+#define ERROR(msg) LOG(PURPLE, "ERROR", msg)
+#define FATAL(msg)           \
+  do {                       \
+    LOG(RED, "FATAL", msg);  \
+    std::exit(EXIT_FAILURE); \
+  } while (0)
+
+// NOLINTEND
